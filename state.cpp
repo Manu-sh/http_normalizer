86194719d2fa385076ec6fc7ae837473af60ec4b
manu-sh <s3gmentationfault@gmail.com>
2020-05-01 03:01:24 +0200

    optimization

--- state.cpp
+++ state.cpp
@@ -2,8 +2,9 @@
 #include <string>
 #include <vector>
 #include <array>
+#include <cstdint>
 
-enum: unsigned char {
+enum: uint_fast8_t {
 	PROTO,
 	HOST,
 	PORT,
@@ -15,10 +16,12 @@ enum: unsigned char {
 
 std::vector<std::string> parse(const std::string &http_url) {
 
-	const static std::array<int(*const)(char), FRAG+1> fun {
+	using state_t = uint_fast8_t;
 
-		[] ([[maybe_unused]] char c) -> int { return 0xdeadbeef; }, // NOP
-		[] (char c) -> int {
+	const static std::array<state_t(*const)(char), FRAG+1> fun {
+
+		[] ([[maybe_unused]] char c) -> state_t { return 0xff; }, // NOP
+		[] (char c) -> state_t {
 			switch (c) {
 				case ':': return PORT;
 				case '/': return PATH;
@@ -29,7 +32,7 @@ std::vector<std::string> parse(const std::string &http_url) {
 			return HOST;
 		},
 
-		[] (char c) -> int {
+		[] (char c) -> state_t {
 			switch (c) {
 				case '/': return PATH;
 				case '?': return QUERY;
@@ -39,7 +42,7 @@ std::vector<std::string> parse(const std::string &http_url) {
 			return PORT;
 		},
 
-		[] (char c) -> int {
+		[] (char c) -> state_t {
 			switch (c) {
 				case '?': return QUERY;
 				case '#': return FRAG;
@@ -48,23 +51,23 @@ std::vector<std::string> parse(const std::string &http_url) {
 			return PATH;
 		},
 
-		[] (char c) -> int { return c != '#' ? QUERY : FRAG; },
-		[] ([[maybe_unused]] char c) -> int { return FRAG; } // NOP
+		[] (char c) -> state_t { return c != '#' ? QUERY : FRAG; },
+		[] ([[maybe_unused]] char c) -> state_t { return FRAG; } // NOP
 	};
 
 	std::vector<std::string> parts(FRAG+1, "");
 	size_t istr = 0;
-	int status = HOST;
 
 	if (const size_t index = http_url.find("://"); index != std::string::npos) {
 		parts.at(PROTO) = http_url.substr(0, index);
 		istr = index + 3;
 	}
-	
+
+	state_t status = HOST;
 	for (const auto len = http_url.length(); istr < len; ++istr) {
 
 		const char c = http_url.at(istr);
-		const int n_status = fun.at(status)(c);
+		const state_t n_status = fun.at(status)(c);
 
 		if (n_status != status) {
 			status = n_status;
