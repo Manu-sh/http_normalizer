98a2a82442762288bbf87071e1195718712e5686
Manu-sh <s3gmentationfault@gmail.com>
2020-05-13 19:01:19 +0200

    reorganization

---
+++ include/extra.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <http_normalizer.hpp>
+
+#include <memory>
+#include <string>
+
+ // cout << *to_abs("/", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
+ // return: https://duckduckgo.com/s!%2F/?ia=web&q=hsad&t=ffab
+
+// arguments: base_url, current_page_url, href_content
+static inline std::shared_ptr<const std::string> to_abs(const std::string &node_base, const std::string &node_url, const std::string &anchor, int HTTP_NORMALIZER_FLAGS = 0) {
+
+    std::string cpy;
+
+    // TODO: prende anche mailto:x@y.com
+    static const auto &is_relative = [HTTP_NORMALIZER_FLAGS] (const std::string &s) -> bool {
+        return s[0] == '/' || s[0] == '.' || http_normalizer::normalize(s, HTTP_NORMALIZER_FLAGS) == nullptr;
+    };
+
+    if (is_relative(anchor)) {
+        try { http_normalizer n{node_url, HTTP_NORMALIZER_FLAGS}; cpy = is_relative(node_base) ? *n.proto() + "://" + *n.hostname() + '/' + node_base + '/' + anchor : node_base + '/' + anchor; }
+        catch (...) { return nullptr; }
+    } else {
+        cpy = anchor;
+    }
+
+    auto shp = http_normalizer::normalize(cpy, HTTP_NORMALIZER_FLAGS);
+    if (!(shp = shp ? shp : http_normalizer::normalize( node_base + "/" + cpy, HTTP_NORMALIZER_FLAGS)))
+        return nullptr;
+
+    return shp;
+}
+
+/*
+	for (const auto &s : parts)
+		cout << s << endl;
+
+	cout << *to_abs("/", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
+	// http://google.com/sadlife/?ia=web&t=ffab&x=y//s!//%3Fq
+	cout << *to_abs("google.com/sadlife/?x=y", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
+*/
--- tests/http_normalizer_test.cpp
+++ tests/http_normalizer_test.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT
-
 #include <http_normalizer.hpp>
 #include <http_tokenizer.hpp>
+#include <extra.hpp>
 
 #include <doctest/doctest.h>
 
@@ -13,54 +13,18 @@ DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_BEGIN
 #include <cstdint>
 DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_END
 
-
 // constexpr static int HTTP_NORMALIZER_FLAGS = http_parts::OPT::HOSTNAME_STRIP_ALL_PREFIX_WWW | http_parts::OPT::PATH_REMOVE_DIRECTORY_INDEX;
 constexpr static int HTTP_NORMALIZER_FLAGS = 0;
-
- // cout << *to_abs("/", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
- // return: https://duckduckgo.com/s!%2F/?ia=web&q=hsad&t=ffab
-
-// arguments: base_url, current_page_url, href_content
-static inline std::shared_ptr<const std::string> to_abs(const std::string &node_base, const std::string &node_url, const std::string &anchor) {
-
-    std::string cpy;
-
-    // TODO: prende anche mailto:x@y.com
-    static const auto &is_relative = [] (const std::string &s) -> bool {
-        return s[0] == '/' || s[0] == '.' || http_normalizer::normalize(s, HTTP_NORMALIZER_FLAGS) == nullptr;
-    };
-
-    if (is_relative(anchor)) {
-        try { http_normalizer n{node_url, HTTP_NORMALIZER_FLAGS}; cpy = is_relative(node_base) ? *n.proto() + "://" + *n.hostname() + '/' + node_base + '/' + anchor : node_base + '/' + anchor; }
-        catch (...) { return nullptr; }
-    } else {
-        cpy = anchor;
-    }
-
-    auto shp = http_normalizer::normalize(cpy, HTTP_NORMALIZER_FLAGS);
-    if (!(shp = shp ? shp : http_normalizer::normalize( node_base + "/" + cpy, HTTP_NORMALIZER_FLAGS)))
-        return nullptr;
-
-    return shp;
-}
-
-/*
-	for (const auto &s : parts)
-		cout << s << endl;
-
-	cout << *to_abs("/", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
-	// http://google.com/sadlife/?ia=web&t=ffab&x=y//s!//%3Fq
-	cout << *to_abs("google.com/sadlife/?x=y", "https://dUckduckgo.com", "/s%21%2f/?q=hsad&t=ffab&ia=web") << endl;
-*/
 
 using namespace std;
 
 TEST_CASE("testing http_tokenizer::normalizer()") {
 
     // TODO: #fragmet silently stripped away
+    // TODO: a single label hostname not allowed
+
     // REQUIRE(*http_normalizer::normalize("http://hostname:8/path/?x=1&y=2#frag", HTTP_NORMALIZER_FLAGS) == "http://hostname:8/path/?x=1&y=2");
 
-    // TODO: a single label hostname not allowed
     REQUIRE(*http_normalizer::normalize("http://hostname.x:8/path/?x=1&y=2#frag", HTTP_NORMALIZER_FLAGS) == "http://hostname.x:8/path/?x=1&y=2");
     REQUIRE(*http_normalizer::normalize("hTt%50S://hostname.x:8/path/?x=1&y=2#frag", HTTP_NORMALIZER_FLAGS) == "https://hostname.x:8/path/?x=1&y=2");
 
