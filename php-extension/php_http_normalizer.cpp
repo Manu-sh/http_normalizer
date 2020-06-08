e353e354e1c89bb75f226e822bb3ce7adc86a265
Manu-sh <s3gmentationfault@gmail.com>
2020-06-08 15:15:07 +0200

    wip

--- php-extension/php_http_normalizer.cpp
+++ php-extension/php_http_normalizer.cpp
@@ -1,8 +1,19 @@
 #include <http_normalizer/public/http_normalizer.hpp>
+#include <http_normalizer/public/extra.hpp>
+
 #include <phpcpp.h>
 
 // constexpr static int HTTP_NORMALIZER_FLAGS = http_parts::OPT::HOSTNAME_STRIP_ALL_PREFIX_WWW | http_parts::OPT::PATH_REMOVE_DIRECTORY_INDEX;
 constexpr static int HTTP_NORMALIZER_FLAGS = 0;
+
+Php::Value http_normalize_to_abs(Php::Parameters &params) {
+    Php::Value node_base_str = params[0];
+    Php::Value node_url_str = params[1];
+    Php::Value node_anchor_str = params[2];
+
+    const auto &shp = to_abs(node_base_str, node_url_str, node_anchor_str, ::HTTP_NORMALIZER_FLAGS);
+    return shp ? *shp : "";
+}
 
 Php::Value http_normalize(Php::Parameters &params) {
 	Php::Value string_val = params[0];
@@ -11,13 +22,21 @@ Php::Value http_normalize(Php::Parameters &params) {
 }
 
 extern "C" {
+
     PHPCPP_EXPORT void *get_module() {
         static Php::Extension extension("libphp_http_normalizer", "1.0");
         
+        extension.add<http_normalize_to_abs>("http_normalize_to_abs", {
+            Php::ByVal("input_part_0", Php::Type::String, true),
+            Php::ByVal("input_part_1", Php::Type::String, true),
+            Php::ByVal("input_part_2", Php::Type::String, true)
+        });
+
         extension.add<http_normalize>("http_normalize", {
             Php::ByVal("input_part", Php::Type::String, true)
         });
 
         return extension;
     }
+
 }
