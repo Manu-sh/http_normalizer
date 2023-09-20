41367b4b80d8b4cbab834abf5901c79b1e7eb669
Emanuele Innamorati <s3gmentationfault@gmail.com>
2023-09-21 00:32:17 +0200

    Update http_normalizer.hpp

--- http_normalizer/include/http_normalizer/public/http_normalizer.hpp
+++ http_normalizer/include/http_normalizer/public/http_normalizer.hpp
@@ -15,7 +15,8 @@ class http_normalizer { /* http url parser/normalizer */
 		enum : unsigned char {
 			HOSTNAME_STRIP_ALL_PREFIX_WWW = 1, // normalize_hostname_opt
 			PATH_REMOVE_DIRECTORY_INDEX   = 1 << 1,
-			KEEP_FRAGMENT                 = 1 << 2
+			PATH_KEEP_TRAILING_SLASH      = 1 << 2,
+			KEEP_FRAGMENT                 = 1 << 3,
 		};
 
 		explicit http_normalizer(const std::string &u, int flags = 0);
